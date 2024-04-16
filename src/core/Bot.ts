2b566e014370a28991b7b0cfcc367bb7dd45030f
Vtchoo <vittor-limma@hotmail.com>
2024-04-16 13:33:07 -0300

    add function to save message scores and get user scores

--- src/core/Bot.ts
+++ src/core/Bot.ts
@@ -1,8 +1,21 @@
-import Discord, { GuildMember } from 'discord.js'
+import Discord, { GuildMember, User } from 'discord.js'
 import { UserRole } from '../models/GuildMember'
 import { getUsersRepository } from '../repositories/GuildMembersRepository'
 import { getGuildsRepository } from '../repositories/GuildsRepository'
 import { Guild } from '../models/Guild'
+import MessageScore from '../models/MessageScore'
+import dataSource from '../../dataSource'
+import getMessageScoresRepository from '../repositories/MessageScoresRepository'
+
+interface GetUserScoreContext {
+    requestedBy: GuildMember
+}
+
+export class UserNotFoundError extends Error {
+    constructor() {
+        super('User not found')
+    }
+}
 
 class Bot {
     
@@ -56,6 +69,78 @@ class Bot {
             console.log(error)
             throw error
         }
+    }
+
+    async saveMessageScores(messages: MessageScore[]) {
+
+        const queryRunner = dataSource.createQueryRunner()
+
+        try {
+
+            // Calculate user scores
+            const userScores = messages.reduce((acc, curr) => {
+                const user = acc.find(u => u.id === curr.userId)
+
+                if (user) {
+                    user.score += curr.score
+                } else {
+                    acc.push({
+                        id: curr.userId,
+                        guildId: curr.guildId,
+                        score: curr.score
+                    })
+                }
+
+                return acc
+            }, [] as { id: string, score: number, guildId: string }[])
+
+
+            await queryRunner.startTransaction()
+
+            // Increment user scores
+            const userRepository = getUsersRepository(queryRunner.manager)
+            await Promise.all(userScores.map(async user => {
+                await userRepository.increment({ id: user.id, guildId: user.guildId }, 'score', user.score)
+            }))
+
+            // Save message scores
+            const messageScoreRepository = getMessageScoresRepository(queryRunner.manager)
+
+            const negativeMessages = messages.filter(m => m.score < 0)
+
+            const messageScores = messageScoreRepository.create(negativeMessages)
+            await messageScoreRepository.save(messageScores)
+
+            await queryRunner.commitTransaction()
+        } catch (error) {
+            console.log(error)
+            await queryRunner.rollbackTransaction()
+            throw error
+        } finally {
+            await queryRunner.release()
+        }
+    }
+
+    async getUserScore(userId: string, guildId: string, context: GetUserScoreContext) {
+
+        const repository = getUsersRepository()
+
+        const guildMember = await repository.findOne({ where: { id: userId, guildId } })
+
+        if (!guildMember)
+            throw new UserNotFoundError()
+
+        if (context.requestedBy.id !== userId)
+            return { userId, score: guildMember.score, globalScore: null }
+
+        const globalScore = await repository.sum('score', { id: userId })
+
+        return {
+            userId,
+            score: guildMember.score,
+            globalScore: globalScore
+        }
+
     }
 }
 
